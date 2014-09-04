6077138a7e5a000ce2724ae2d28523ef3935aab4
Alexandre Delanoë <alexandre@delanoe.org>
2014-09-04 14:52:49 +0200

    adding grappelli urls

--- gargantext_web/urls.py
+++ gargantext_web/urls.py
@@ -7,6 +7,18 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'gargantext_web.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^grappelli/', include('grappelli.urls')), # grappelli URLS
     url(r'^admin/', include(admin.site.urls)),
 )
+
+from django.conf import settings
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.STATIC_ROOT,
+        }),
+)
+

f1a0571248ed39b4a45006b1d93a143cb92168f3
Alexandre Delanoë <alexandre@delanoe.org>
2014-10-04 16:51:34 +0200

    adding nlp part

--- analysis/extract.py
+++ analysis/extract.py
@@ -24,6 +24,77 @@ def words_field(corpus=None, field='abstract'):
     
     docs = Document.objects.filter(corpus=corpus)
     
+    if corpus.language == 'Français':
+        from .languages import french_pos_tag as pos_tag
+        from .languages import french_stem as stem
+
+    elif corpus.language == 'English':
+        from .languages import english_pos_tag as pos_tag
+        from .languages import english_stem as stem
+
+    
+    def fouille(text, grammar_rule='jj_nn'):
+        # TODO: grammar_rule
+        from .grammar_rules import jj_nn as rule
+        grammar = nltk.RegexpParser(rule)
+        
+        text = clean(text)
+        sentances = nltk.sent_tokenize(text)
+        result = []
+        
+        for sentance in sentances:
+            try:
+                t = pos_tag(sentance)
+                g = grammar.parse(t)
+                x = g.subtrees()
+            
+                while True:
+                    try:
+                        subtree = next(x)
+                        if subtree.label() == 'NP':
+                            #print(subtree.label())
+                            result.append(subtree.leaves())
+                            
+                    except Exception as e:
+                        break
+                        
+            except Exception as e:
+                print(e)
+                pass
+                
+        return iter(result)
+
+    if True:
+    #if isinstance(corpus, Corpus):
+        for doc in corpus:
+            text = doc.get(key, '')
+            u_id = doc.get(unique_id, 'XX')
+            date = doc.get('date', 'XX')
+
+            ngrams = fouille(text)
+            
+            while True:
+                try:
+                    leave = next(ngrams)
+                    words = stems(leave)
+                    s = words[0]
+                    w  = tuple(words[1])
+
+                    if s not in set(self.stems.keys()):
+                        self.stems[s]['synonyms']                = set()
+                        self.stems[s]['frequency'][date][u_id]   = 0
+        # TODO c-value, nonograms and ngrams
+                        
+                        if w not in self.stems[s]['synonyms']:
+                            self.stems[s]['synonyms'].add(w)
+                        
+                    self.stems[s]['frequency'][date][u_id] += 1
+                
+                except Exception as e:
+                    #print('h',e)
+                    break
+
+
     for doc in docs:
         try:
             sentences = nltk.sent_tokenize(doc.abstract)
@@ -71,12 +142,12 @@ def words_field(corpus=None, field='abstract'):
                  delete from documents_ngramdocumenttemporary;
              """
     cursor.execute(query_string)
-    
+
 
 def words_fields(corpus=None, fields=['title',]):
     try:
         for field in fields:
             words_field(corpus=corpus, field=field)
     except Exception as e: print(e)
-    
+
 

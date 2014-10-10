3f83dbc945fd110241c75b8b2aab847b1b9f29d2
Alexandre Delanoë <alexandre@delanoe.org>
2014-10-10 18:17:17 +0200

    adding liste function to Node class (not tested yet)

--- node/models.py
+++ node/models.py
@@ -30,6 +30,10 @@ class Node(MP_Node):
     def __str__(self):
         return self.name
 
+    def liste(self, user):
+        for noeud in Node.objects.filter(user=user):
+            print(noeud.depth * "    " + "[%d] %d" % (noeud.pk, noeud.name))
+
 class Project(Node):
     class Meta:
         proxy=True

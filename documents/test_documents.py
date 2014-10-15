3d6a4f8f46ef41553a51b75977eaaa49f0dd35cd
Alexandre Delanoë <alexandre@delanoe.org>
2014-10-15 17:12:53 +0200

    adding dependencies, document_test place

--- documents/test_documents.py
+++ documents/test_documents.py
@@ -1,5 +1,5 @@
 
-
+from django.db import transaction
 from django.contrib.auth.models import User
 from documents.models import Project, Corpus, Document, Source, Language
 from sources import importateur

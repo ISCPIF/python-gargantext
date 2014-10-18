4394900ca9bd79d3aceac407cfcc137b3618c042
Mathieu Rodic <mathieu@rodic.fr>
2014-10-18 12:56:52 +0200

    Changed the taggers to facilitate their use. See test.py for examples.

--- mat-parsing/Taggers/NltkTagger.py
+++ mat-parsing/Taggers/NltkTagger.py
@@ -5,13 +5,6 @@ import nltk
 
 class NltkTagger(Tagger):
     
-    def send_tokens(self, tokens):
-        self.buffer += nltk.pos_tag(tokens)
+    def tag_tokens(self, tokens, single=True):
+        return nltk.pos_tag(tokens)
 
-
-
-# tagger = NltkTagger()
-# tagger.start()
-# # tagger.send_text("This is not a sentence. Or, is it? I wish it was; I could perform tagging tests on it.")
-# tagger.send_text("This is not a sentence.")
-# print(tagger.end())
--- mat-parsing/Taggers/TreeTagger.py
+++ mat-parsing/Taggers/TreeTagger.py
@@ -23,16 +23,20 @@ _tag_replacements = identity_dict({
     # Do we also have to take semicolons, comas and other points into account?
 })
 
-
 def _readOutput(output, buffer):
+    hasStarted = False
     while True:
         line = output.readline()
         if line:
-            if line == b"<end/>\n":
+            if line == b"<block>\n":
+                hasStarted = True
+                continue
+            if line == b"<block/>\n":
                 break
-            token, tag = line.decode('utf8').split()[:2]
-            tag = _tag_replacements[tag.split(':')[0]]
-            buffer.append((token, tag))
+            if hasStarted:
+                token, tag = line.decode('utf8').split()[:2]
+                tag = _tag_replacements[tag.split(':')[0]]
+                buffer.append((token, tag))
         else:
             time.sleep(0.1)
 
@@ -62,26 +66,41 @@ class TreeTagger(Tagger):
         )
         self._input, self._output = self._popen.stdin, self._popen.stdout
         # self._thread = threading.Thread(target=_readOutput, args=(self._output, self.buffer, )).start()
-        self._thread = threading.Thread(target=_readOutput, args=(self._output, self.buffer, ))
-        self._thread.start()
+        # self.buffer = OutputBuffer()
         
-    def send_tokens(self, tokens):
-        for token in tokens:
-            self._input.write(bytes(token + "\n", "utf8"))
-    
-    def end(self):
-        # send some dummy tokens, then wait for the text to be treated
-        self.send_tokens("<end/> Les sanglots longs des violons de l ' automne bercent mon coeur d ' une langueur monotone .".split())
-        # wait for the thread to end
-        self._thread.join()
+    def stop(self):
         # terminates the 'treetagger' process
         self._popen.kill()
         self._popen.terminate()
-        # returns the tagged tokens
+        
+        
+    def tagging_start(self):
+        self.buffer = []
+        self._thread = threading.Thread(target=_readOutput, args=(self._output, self.buffer, ))
+        self._thread.start()
+        self._input.write(b"<block>\n")
+        
+    def tagging_end(self):
+        self._input.write(b"<block/>\n")
+        # sends some dummy tokens, then wait for the text to be treated
+        self.tag_tokens("Les sanglots longs des violons de l ' automne bercent mon coeur d ' une langueur monotone .".split(), False)
+        self._thread.join()
+        
+        
+    def tag_tokens(self, tokens, single=True):
+        if single:
+            self.tagging_start()
+        for token in tokens:
+            self._input.write(bytes(token + "\n", "utf8"))
+        if single:
+            self.tagging_end()
+            return self.buffer
+            
+    def tag_text(self, text):
+        self.tagging_start()
+        for line in text.split('\n'):
+            tokens = self._re_sentence.findall(line)
+            self.tag_tokens(tokens, False)
+        self.tagging_end()
         return self.buffer
 
-
-# tagger = TreeTagger()
-# tagger.start()
-# tagger.send_text("Ceci n'est pas une phrase, n'est-ce pas? Parfois, il faut tester des phrases ; mêmes celles avec des points-virgules.")
-# print(tagger.end())

0720146f0be82b9edaad88e3f9efe98e99df0d2f
Mathieu Rodic <mathieu@rodic.fr>
2014-10-23 07:18:39 +0200

    [FEATURE] ISI parser - Almost working

--- parsing/FileParsers/IsiFileParser.py
+++ parsing/FileParsers/IsiFileParser.py
@@ -5,28 +5,29 @@ from parsing.FileParsers.FileParser import FileParser
 class IsiFileParser(FileParser):
     
     _parameters = {
-        "ER":   {"type": "delimiter"},
-        "AU":   {"type": "metadata", "key": "authors", "concatenate": False},
-        "AB":   {"type": "metadata", "key": "abstract", "concatenate": True},
+        b"ER":  {"type": "delimiter"},
+        b"TI":  {"type": "metadata", "key": "title", "concatenate": b" "},
+        b"AU":  {"type": "metadata", "key": "authors", "concatenate": b", "},
+        b"AB":  {"type": "metadata", "key": "abstract", "concatenate": b" "},
     }
     
     def parse(self, parentNode=None, tag=True):
         metadata = {}
         last_key = None
         last_values = []
-        for line in self.file:
+        for line in self._file:
             if len(line) > 2:
                 parameter_key = line[:2]
-                if parameter_key != last_key:
-                    if last_key is not None:
+                if parameter_key != b'  ' and parameter_key != last_key:
+                    if last_key in self._parameters:
                         parameter = self._parameters[last_key]
                         if parameter["type"] == "metadata":
-                            metadata[parameter["key"]] = ' '.join(last_values) if parameter["concatenate"] else last_values
-                        elif parameter["type"] == "metadata":
+                            metadata[parameter["key"]] = parameter["concatenate"].join(last_values)
+                        elif parameter["type"] == "delimiter":
                             print(metadata)
                             metadata = {}
                             break
-                    parameter = self._parameters[last_key]
                     last_key = parameter_key
                     last_values = []
                 last_values.append(line[3:-1])
+        self.file.close()

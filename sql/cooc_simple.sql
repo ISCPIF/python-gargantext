4f56f139ff82116e6fbf31a2c61d44fdbd83d029
Alexandre Delanoë <alexandre@delanoe.org>
2014-10-29 14:15:36 +0100

    [TESTS] SQL ajout cooc simple.

---
+++ sql/cooc_simple.sql
@@ -0,0 +1,15 @@
+SELECT 
+x.ngram_id, y.ngram_id, COUNT(*) AS c
+
+FROM
+node_node_ngram AS x
+INNER JOIN 
+node_node_ngram AS y
+ON
+x.node_id = y.node_id
+
+AND
+x.ngram_id <> y.ngram_id
+
+GROUP BY
+x.ngram_id, y.ngram_id

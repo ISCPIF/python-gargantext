52f773290d6aab399efe72f79212d555d19494b8
Alexandre Delanoë <alexandre@delanoe.org>
2014-12-01 21:22:15 +0100

    [INIT] Dependences à installer script facile.

--- init/dependances.sh
+++ init/dependances.sh
@@ -1,39 +1,30 @@
-
-
 sudo apt-get install python-virtualenv
-sudo apt-cache search libpng
 sudo apt-get install libpng12-dev
 sudo apt-get install libpng-dev
-apt-cache search freetype
-apt-cache search freetype | grep dev
+sudo apt-cache search freetype
 sudo apt-get install libfreetype6-dev
 sudo apt-cache search python-dev
 sudo apt-get install python-dev
 sudo apt-get install libpq-dev
-
-
-
-postgresql-contrib
-
-libpq-dev
+sudo apt-get postgresql-contrib
+sudo aptèget install libpq-dev
 # rajouter david
 #
 #
 #Paquets Debian a installer
 # easy_install -U distribute (matplotlib)
 #lxml
-libffi-dev
-libxml2-dev
-libxslt1-dev
+sudo apt-get install libffi-dev
+sudo apt-get install libxml2-dev
+sudo apt-get install libxslt1-dev
 
 # ipython readline
-libncurses5-dev
-pandoc
-
+sudo apt-get install libncurses5-dev
+sudo apt-get install pandoc
 
 # scipy:
-gfortran
-libopenblas-dev
-liblapack-dev
+sudo apt-get install gfortran
+sudo apt-get install libopenblas-dev
+sudo apt-get install liblapack-dev
 
 

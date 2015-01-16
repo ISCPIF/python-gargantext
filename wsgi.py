b28dd82b48b03f83e8057832a2c4731ce4fcf799
Alexandre Delanoe <alexandre@delanoe.org>
2015-01-16 06:07:58 +0100

    [ADMIN] Tina configuration.

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,17 @@
 """
-WSGI config for gargantext_web project.
+WSGI config for gargantext project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+sys.path.append('/srv/gargantext')
+sys.path.append('/srv/gargantext/gargantext_web')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gargantext_web.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+

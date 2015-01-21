c006ba3b46ba6c21ba9bc217adea57094caa2a77
Alexandre Delanoë <alexandre@delanoe.org>
2015-01-21 12:31:44 +0100

    [ADMIN] Settings for stable (diff with tina)

--- wsgi.py
+++ wsgi.py
@@ -1,17 +1,14 @@
 """
-WSGI config for gargantext project.
+WSGI config for gargantext_web project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
-import os, sys
-sys.path.append('/srv/gargantext')
-sys.path.append('/srv/gargantext/gargantext_web')
+import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gargantext_web.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-

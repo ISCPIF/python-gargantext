5c5941614f7279063967647f9ac0c41fdfc1fac5
Alexandre Delanoe <alexandre@delanoe.org>
2015-01-21 12:07:50 +0100

    [BUG FIX] init accounts.

--- init_accounts.py
+++ init_accounts.py
@@ -58,8 +58,8 @@ def mines_account_creation(fichier=None):
         fichier = "/home/alexandre/projets/forccast/Tutorat/2014-2015/comptes_gargantext.csv"
     accounts = open(fichier, "r")
     for line in accounts.readlines():
-        username, email, password = line.split(',')
-        create_user(username, email, password=password, notify=True)
+        username, email, password, fin = line.split(',')
+        create_user(username, email, password=password, notify=False)
         #delete_user(username)
     accounts.close()
 

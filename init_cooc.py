bd4666e646726b5ff04c3ee9bda15cb4c2212c77
Alexandre Delanoë <alexandre@delanoe.org>
2015-01-27 11:19:10 +0100

    [FEAT] Script to reinit cooc nodes.

---
+++ init_cooc.py
@@ -0,0 +1,24 @@
+# Without this, we couldn't use the Django environment
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gargantext_web.settings")
+os.environ.setdefault("DJANGO_HSTORE_GLOBAL_REGISTER", "False")
+
+
+# We're gonna use all the models!
+
+from node.models import User, NodeType, Node
+
+
+user = User.objects.get(username = 'alexandre')
+
+# Reset: all data
+
+try:
+    typeDoc     = NodeType.objects.get(name='Cooccurrence')
+except Exception as error:
+    print(error)
+
+Node.objects.filter(user=user, type=typeDoc).all().delete()
+
+exit()

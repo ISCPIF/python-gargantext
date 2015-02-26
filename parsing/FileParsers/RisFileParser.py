0426d57eef4fc453c9574ee783d16852ef2028c4
Mathieu Rodic <mathieu@rodic.fr>
2015-02-26 11:04:39 +0100

    [BUGFIX] Last metadata of RIS and ISI files are now appended to the corpus https://forge.iscpif.fr/issues/1368

--- parsing/FileParsers/RisFileParser.py
+++ parsing/FileParsers/RisFileParser.py
@@ -20,29 +20,31 @@ class RisFileParser(FileParser):
         metadata = {}
         last_key = None
         last_values = []
+        # browse every line of the file
         for line in file:
             if len(line) > 2:
+                # extract the parameter key
                 parameter_key = line[:2]
                 if parameter_key != b'  ' and parameter_key != last_key:
                     if last_key in self._parameters:
+                        # translate the parameter key
                         parameter = self._parameters[last_key]
                         if parameter["type"] == "metadata":
                             separator = parameter["separator"] if "separator" in parameter else ""
                             metadata[parameter["key"]] = separator.join(last_values)
                         elif parameter["type"] == "delimiter":
-                            try:
-                                if 'language_fullname' not in metadata.keys():
-                                    if 'language_iso3' not in metadata.keys():
-                                        if 'language_iso2' not in metadata.keys():
-                                            metadata['language_iso2'] = 'en'
-                                yield metadata
-                                metadata = {}
-                            except:
-                                pass
+                            if 'language_fullname' not in metadata.keys():
+                                if 'language_iso3' not in metadata.keys():
+                                    if 'language_iso2' not in metadata.keys():
+                                        metadata['language_iso2'] = 'en'
+                            yield metadata
+                            metadata = {}
                     last_key = parameter_key
                     last_values = []
                 try:
                     last_values.append(line[3:-1].decode())
                 except Exception as error:
                     print(error)
-                    pass
+        # if a metadata object is left in memory, yield it as well
+        if metadata:
+            yield metadata

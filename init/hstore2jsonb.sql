26261382dc546d741ebc47430bf6b06d61a90faf
Alexandre Delanoë <alexandre@delanoe.org>
2015-04-01 16:54:57 +0200

    [ADMIN] towards jsonb.

--- init/hstore2jsonb.sql
+++ init/hstore2jsonb.sql
@@ -1,27 +1,14 @@
-ALTER TABLE ONLY node_node
-ALTER COLUMN metadata
-DROP NOT NULL
-;
-
-ALTER TABLE ONLY node_node
-ALTER COLUMN metadata
-DROP DEFAULT
-;
-
-ALTER TABLE ONLY node_node
-ALTER COLUMN metadata
-TYPE JSON
-USING hstore_to_json(metadata)
-;
-
-ALTER TABLE ONLY node_node
-ALTER COLUMN metadata
-SET DEFAULT '{}'::json
-;
 
 
-ALTER TABLE ONLY node_node
-ALTER COLUMN metadata
-SET NOT NULL
-;
+ALTER TABLE ONLY node_node ALTER COLUMN date SET DEFAULT CURRENT_DATE ;
+
+ALTER TABLE ONLY node_node ALTER COLUMN metadata DROP NOT NULL ;
+
+ALTER TABLE ONLY node_node ALTER COLUMN metadata DROP DEFAULT ;
+
+ALTER TABLE ONLY node_node ALTER COLUMN metadata TYPE JSONB USING hstore_to_json(metadata)::jsonb ;
+
+ALTER TABLE ONLY node_node ALTER COLUMN metadata SET DEFAULT '{}'::jsonb ;
+
+ALTER TABLE ONLY node_node ALTER COLUMN metadata SET NOT NULL ;
 

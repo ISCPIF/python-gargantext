cdbbebb35a448f16faacc73e6f6f57b40b530196
Alexandre Delanoë <alexandre@delanoe.org>
2015-04-13 17:48:15 +0200

    [ADMIN] Django config

--- init/install/3-librairies.sh
+++ init/install/3-librairies.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
 
-cd /srv/
+cd /tmp/
 wget http://docs.delanoe.org/gargantext_lib.tar.bz2
-sudo tar xvjf gargantext_lib.tar.bz2
-sudo chown gargantua:gargantua /srv/gargantext_lib
+cd /srv/
+sudo mkdir gargantext_lib
+sudo chown -R gargantua:www-data /srv/gargantext_lib
+
+tar xvjf /tmp/gargantext_lib.tar.bz2
+sudo chown -R gargantua:www-data /srv/gargantext_lib
 
 cd /srv/gargantext_lib/js
 git clone git@github.com:PkSM3/garg.git
-
-# Installing TreeTagger
-
-cp treetagger /srv/gargantext/parsing/Tagger/treetagger
-cp nlpserver /srv/gargantext/parsing/Tagger/
-
-# Installing nlpserver
-
 

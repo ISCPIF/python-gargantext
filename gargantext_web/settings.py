e28de1cd8bcd1342b69f3b7542d52397303f233c
Alexandre Delanoë <alexandre@delanoe.org>
2015-04-14 17:15:05 +0200

    [FIX] allowed on local network at isc.

--- gargantext_web/settings.py
+++ gargantext_web/settings.py
@@ -74,6 +74,8 @@ ALLOWED_HOSTS = ['localhost',
                 'stable.gargantext.org', 
                 'iscpif.gargantext.org', 
                 'mines.gargantext.org', 
+                'garg-dev.iscpif.fr', 
+                'garg-stable.iscpif.fr', 
                 'dev.gargantext.org', 
                 'beta.gargantext.org']
 

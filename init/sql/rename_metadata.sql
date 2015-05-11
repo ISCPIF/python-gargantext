4a03e2381bebc79f97e110bea441647b2b89c648
Alexandre Delanoë <alexandre@delanoe.org>
2015-05-11 17:59:28 +0200

    [ADMIN] sql table/column rename

--- init/sql/rename_metadata.sql
+++ init/sql/rename_metadata.sql
@@ -3,8 +3,6 @@ ALTER TABLE node_node RENAME metadata TO hyperdata ;
 
 ALTER TABLE node_metadata RENAME TO node_hyperdata ;
 
-ALTER TABLE node_node_metadata 
-	RENAME TO node_node_hyperdata 
-	RENAME metadata_id TO  hyperdata_id 
-	;
+ALTER TABLE node_node_metadata RENAME TO node_node_hyperdata ;
+ALTER TABLE node_node_metadata RENAME metadata_id TO  hyperdata_id ;
 

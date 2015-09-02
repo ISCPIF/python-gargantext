3b565087401948d99145965f7e73457da3bf3d8f
Alexandre Delanoë <anoe@debian.org>
2015-09-02 19:04:00 +0200

    [FEAT] Configuring Melt but still with bugs.

--- parsing/NgramsExtractors/FrenchNgramsExtractor.py
+++ parsing/NgramsExtractors/FrenchNgramsExtractor.py
@@ -1,9 +1,9 @@
 from .NgramsExtractor import NgramsExtractor
-from ..Taggers import TreeTagger
+from ..Taggers import TreeTagger, MeltTagger
 
 
 class FrenchNgramsExtractor(NgramsExtractor):
 
     def start(self):
-        self.tagger = TreeTagger()
-        # self.tagger = MeltTagger(language='fr')
+        #self.tagger = TreeTagger()
+        self.tagger = MeltTagger(language='fr')
--- parsing/NgramsExtractors/NgramsExtractor.py
+++ parsing/NgramsExtractors/NgramsExtractor.py
@@ -14,22 +14,22 @@ class NgramsExtractor:
         self._label = "NP"
         self._rule = self._label + ": " + rule
         self._grammar = nltk.RegexpParser(self._rule)
-        
+
     def __del__(self):
         self.stop()
-        
+
     def start(self):
         self.tagger = TurboTagger()
-        
+
     def stop(self):
         pass
-        
-    
+
+
     """Extracts a list of ngrams.
     Returns a list of the ngrams found in the given text.
     """
     def extract_ngrams(self, contents):
-        tagged_ngrams = self.tagger.tag_text(contents)
+        tagged_ngrams = list(self.tagger.tag_text(contents))
         if len(tagged_ngrams):
             grammar_parsed = self._grammar.parse(tagged_ngrams)
             for subtree in grammar_parsed.subtrees():

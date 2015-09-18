303ea3a17a20520a1391004b93e16f76ca656fe4
Alexandre Delanoë <anoe@debian.org>
2015-09-18 17:23:20 +0200

    [DOC] Documentation of the functions.

--- ngram/cvalue.py
+++ ngram/cvalue.py
@@ -15,21 +15,14 @@ from sqlalchemy import desc, asc, or_, and_, Date, cast, select
 from sqlalchemy import literal_column
 from sqlalchemy.orm import aliased
 
-
 #from testlists import *
 from math import log
 from functools import reduce
 
-
-#terms =   {'adenoic cystic basal cell carcinoma' : 5
-#           , 'cystic basal cell carcinoma' : 11
-#           , 'ulcerated basal cell carcinoma' : 7
-#           , 'recurrent basal cell carcinoma' : 5
-#           , 'circumscribed basal cell carcinoma' : 3
-#           , 'basal cell carcinoma' : 984
-#           }
-
 def getNgrams(corpus=None, limit=100):
+    '''
+    getNgrams :: Corpus -> [(Int, String, String, Float)]
+    '''
     terms = dict()
     tfidf_node = get_or_create_node(nodetype='Tfidf (global)'
                                     , user_id=corpus.user_id
@@ -46,6 +39,16 @@ def getNgrams(corpus=None, limit=100):
                         .limit(limit)
           )
 
+    '''
+    only for tests if needed (see scientific paper)
+    ngrams =   {'adenoic cystic basal cell carcinoma' : 5
+               , 'cystic basal cell carcinoma' : 11
+               , 'ulcerated basal cell carcinoma' : 7
+               , 'recurrent basal cell carcinoma' : 5
+               , 'circumscribed basal cell carcinoma' : 3
+               , 'basal cell carcinoma' : 984
+               }
+    '''
     for n in ngrams:
         try:
             nn = list(n)
@@ -56,14 +59,25 @@ def getNgrams(corpus=None, limit=100):
 
 
 def computeCvalue(corpus=None):
+    '''
+    computeCvalue :: Corpus
+    frequency :: String -> Int -> Int
+    includes :: String -> String -> Bool
+
+    included :: String -> [String] -> [String]
+    (The result is lazy)
+
+    string2freq :: [String] -> [Int]
+    sumAll :: [Int] -> Int
+    cvalue :: String -> Float
+    '''
     terms = getNgrams(corpus=corpus)
 
-# frequency :: String -> Int -> Int
     def frequency(word, source=terms):
-        #terms = getNgrams(corpus_i)
         return(source.get(word,0)[1])
+        #terms = getNgrams(corpus_i)
 
-# includes :: String -> String -> Bool
+
     def includes(string1,string2):
         if string1 == string2:
             return(False)
@@ -72,20 +86,19 @@ def computeCvalue(corpus=None):
             s2 = set(string2.split(' '))
             return(s1 == s1.intersection(s2))
 
-# included :: String -> [String] -> [String]
-# (The result is lazy)
+
     def included(string,listofstring):
         return(filter(lambda x: includes(string,x), listofstring))
 
-# string2freq :: [String] -> [Int]
+
     def string2freq(listofstring, f=frequency):
         return(map(lambda x: f(x), listofstring))
 
-# sumAll :: [Int] -> Int
+
     def sumAll(listofint):
         return(reduce(lambda x,y: x+y, listofint))
 
-# cvalue :: String -> Float
+
     def cvalue(string, listofstring=list(terms.keys())):
         l = [i for i in included(string,listofstring)]
         stringlog = log(len(string.split(' ')),2)

5a08c859dd873690f05263a01f63e9bf2e1cd239
Alexandre Delanoë <anoe@debian.org>
2015-09-22 16:08:56 +0200

    [FEAT] keeping turbo parser

--- parsing/NgramsExtractors/__init__.py
+++ parsing/NgramsExtractors/__init__.py
@@ -1,5 +1,5 @@
 from .FrenchNgramsExtractor import FrenchNgramsExtractor
-#from .TurboNgramsExtractor import TurboNgramsExtractor as EnglishNgramsExtractor
-from .EnglishNgramsExtractor import EnglishNgramsExtractor
+from .TurboNgramsExtractor import TurboNgramsExtractor as EnglishNgramsExtractor
+#from .EnglishNgramsExtractor import EnglishNgramsExtractor
 from .NgramsExtractor import NgramsExtractor
 

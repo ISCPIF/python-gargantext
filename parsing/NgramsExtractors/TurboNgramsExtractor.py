18b05727348666e8ab39ab97c9c063eb60aa22df
PkSM3 <pokesam3@gmail.com>
2015-11-23 22:27:14 +0100

    [UPDATE] ngram-table OK (2)

--- parsing/NgramsExtractors/TurboNgramsExtractor.py
+++ parsing/NgramsExtractors/TurboNgramsExtractor.py
@@ -1,8 +1,8 @@
 from .NgramsExtractor import NgramsExtractor
-from ..Taggers import NltkTagger
+from ..Taggers import TurboTagger
 
 
 class TurboNgramsExtractor(NgramsExtractor):
 
     def start(self):
-        self.tagger = NltkTagger()
+        self.tagger = TurboTagger()

185b610d7957a696f33f5394a4fee60fabe384a2
Mathieu Rodic <mathieu@rodic.fr>
2016-02-05 10:47:54 +0100

    [FEAT] finished re-implementing unauthenticated pages

--- gargantext/views/pages/urls.py
+++ gargantext/views/pages/urls.py
@@ -4,8 +4,12 @@ from . import main, auth
 
 
 urlpatterns = [
+    # presentation pages
     url(r'^$', main.home),
     url(r'^about/?$', main.about),
+    # maintenance mode
+    url(r'^maintenance/?$', main.maintenance),
+    # authentication
     url(r'^auth/login/?$', auth.login),
     url(r'^auth/logout/?$', auth.logout),
 ]

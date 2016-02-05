f102b5df4ec9d2015a759af3415b905442c0f982
Mathieu Rodic <mathieu@rodic.fr>
2016-02-05 11:17:09 +0100

    [FEATURE] implemented a decorator for required authentication

--- gargantext/util/http.py
+++ gargantext/util/http.py
@@ -3,4 +3,14 @@ from django.template import Context, RequestContext
 from django.http import Http404, HttpResponse, HttpResponseRedirect, HttpResponseForbidden
 from django.shortcuts import render_to_response,redirect
 
+from django.utils.http import urlencode
+
 from gargantext import settings
+
+
+def requires_auth(func):
+    def _requires_auth(request, *args, **kwargs):
+        if not request.user.is_authenticated():
+            return redirect('/auth/login/?next=%s' % request.path)
+        return func(request, *args, **kwargs)
+    return _requires_auth

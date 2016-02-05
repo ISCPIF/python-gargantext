b35e1b64c2525d9795e720b90dc451ebf54edbff
Mathieu Rodic <mathieu@rodic.fr>
2016-02-05 10:47:54 +0100

    [FEAT] finished re-implementing unauthenticated pages

--- gargantext/views/pages/main.py
+++ gargantext/views/pages/main.py
@@ -5,8 +5,7 @@ from gargantext.util.generators import paragraphs, credits
 
 
 def home(request):
-    '''
-    Home describes the platform.
+    '''Home describes the platform.
     A video draws the narratives.
     If not logged a project test is shown.
     '''
@@ -39,5 +38,20 @@ def about(request):
         'labos': credits.labs(),
         'grants': credits.grants(),
     }))
+
+    return HttpResponse(html)
+
+
+def maintenance(request):
+    '''Gargantext out of service
+    '''
+    template    = get_template('pages/main/maintenance.html')
+    user        = request.user
+    date        = datetime.datetime.now()
+
+    html = template.render(Context({\
+            'user': user,\
+            'date': date,\
+            }))
 
     return HttpResponse(html)

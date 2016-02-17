b9e34eec518b5b0cad98c77fcb20f27bc9ae6bca
Mathieu Rodic <mathieu@rodic.fr>
2016-02-17 19:16:01 +0100

    [FEAT] added database tools for bulk insertion [CODE] removed unused imports

--- gargantext/util/db.py
+++ gargantext/util/db.py
@@ -22,13 +22,107 @@ session = scoped_session(sessionmaker(bind=engine))
 
 # tools to build models
 
-from sqlalchemy.orm import aliased, synonym
 from sqlalchemy.types import *
 from sqlalchemy.schema import Column, ForeignKey, UniqueConstraint
 from sqlalchemy.dialects.postgresql import JSONB
-from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.ext.mutable import MutableDict, MutableList
 
 
-# other useful database stuff
+# useful for queries
 
+from sqlalchemy.orm import aliased
 from sqlalchemy import func
+
+
+# bulk insertions
+
+import psycopg2
+
+def get_cursor():
+    db_settings = settings.DATABASES['default']
+    db = psycopg2.connect(**{
+        'database': db_settings['NAME'],
+        'user':     db_settings['USER'],
+        'password': db_settings['PASSWORD'],
+        'host':     db_settings['HOST'],
+        'port':     db_settings['PORT']
+    })
+    return db, db.cursor()
+
+class bulk_insert:
+
+    def __init__(self, table, keys, data, cursor=None):
+        # prepare the iterator
+        self.iter = iter(data)
+        # template
+        self.template = '%s' + (len(keys) - 1) * '\t%s' + '\n'
+        # prepare the cursor
+        if cursor is None:
+            db, cursor = get_cursor()
+            mustcommit = True
+        else:
+            mustcommit = False
+        # insert data
+        if not isinstance(table, str):
+            table = table.__tablename__
+        cursor.copy_from(self, table, columns=keys)
+        # commit if necessary
+        if mustcommit:
+            db.commit()
+
+    def read(self, size=None):
+        try:
+            return self.template % tuple(
+                str(x).replace('\r', ' ').replace('\n', ' ').replace('\t', ' ').replace("\\","") for x in next(self.iter)
+            )
+        except StopIteration:
+            return ''
+
+    readline = read
+
+
+def bulk_insert_ifnotexists(model, uniquekey, fields, values):
+    db, cursor = get_cursor()
+    # create temporary table with given values
+    sql_columns = 'id INTEGER'
+    for field in fields:
+        column = getattr(model, field)
+        sql_columns += ', %s %s' % (field, column.type, )
+    cursor.execute('CREATE TEMPORARY TABLE __tmp__ (%s)' % (sql_columns, ))
+    bulk_insert('__tmp__', fields, values, cursor=cursor)
+    # update ids of the temporary table
+    cursor.execute('''
+        UPDATE __tmp__
+        SET id = source.id
+        FROM {sourcetable} AS source
+        WHERE __tmp__.{uniquecolumn} = source.{uniquecolumn}
+    '''.format(
+        sourcetable = model.__tablename__,
+        uniquecolumn = uniquekey,
+    ))
+    # insert what has not been found to the real table
+    cursor.execute('''
+        INSERT INTO {sourcetable} ({columns})
+        SELECT {columns}
+        FROM __tmp__
+        WHERE id IS NULL
+    '''.format(
+        sourcetable = model.__tablename__,
+        columns = ', '.join(fields),
+    ))
+    # retrieve dict associating unique key to id
+    cursor.execute('''
+        SELECT source.id, source.{uniquecolumn}
+        FROM {sourcetable} AS source
+        INNER JOIN __tmp__ ON __tmp__.{uniquecolumn} = source.{uniquecolumn}
+    '''.format(
+        sourcetable = model.__tablename__,
+        uniquecolumn = uniquekey,
+        columns = ', '.join(fields),
+    ))
+    result = {
+        row[1]: row[0] for row in cursor.fetchall()
+    }
+    # this is the end!
+    db.commit()
+    return result

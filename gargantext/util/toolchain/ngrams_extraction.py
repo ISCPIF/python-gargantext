0facc254ffacdc83e364b54ea048bfd1d02aa277
Mathieu Rodic <mathieu@rodic.fr>
2016-02-18 09:09:02 +0100

    [BUG] fixed some issues with ngrams extraction [CODE] improved bulk_insert rows management [OPTI] ngrams extraction is treated little by little

--- gargantext/util/toolchain/ngrams_extraction.py
+++ gargantext/util/toolchain/ngrams_extraction.py
@@ -6,32 +6,8 @@ from gargantext.util.ngramsextractors import ngramsextractors
 from collections import defaultdict
 
 
-def extract_ngrams(corpus, rule='{<JJ.*>*<NN.*>+<JJ.*>*}', keys=('title', 'abstract', )):
-    """Extract ngrams for every document below the given corpus.
-    Default language is given by the resource type.
-    The result is then inserted into database.
-    Only fields indicated in `keys` are tagged.
-    """
-    db, cursor = get_cursor()
-    nodes_ngrams_count = defaultdict(int)
-    ngrams_data = set()
-    # extract ngrams
-    resource_type_index = corpus.resources()[0]['type']
-    resource_type = RESOURCETYPES[resource_type_index]
-    default_language_iso2 = resource_type['default_language']
-    for document in corpus.children('DOCUMENT'):
-        for key in keys:
-            value = document.hyperdata.get(key, '')
-            if len(value) == 0:
-                continue
-            # get ngrams
-            language_iso2 = document.hyperdata.get('language_iso2', default_language_iso2)
-            ngramsextractor = ngramsextractors[language_iso2]
-            for ngram in ngramsextractor.extract(value):
-                tokens = tuple(token[0] for token in ngram)
-                terms = ' '.join(tokens)
-                nodes_ngrams_count[(document.id, terms)] += 1
-                ngrams_data.add((terms[:255], len(tokens), ))
+def _integrate_associations(nodes_ngrams_count, ngrams_data, db, cursor):
+    print('INTEGRATE')
     # integrate ngrams
     ngrams_ids = bulk_insert_ifnotexists(
         model = Ngram,
@@ -53,4 +29,44 @@ def extract_ngrams(corpus, rule='{<JJ.*>*<NN.*>+<JJ.*>*}', keys=('title', 'abstr
         cursor = cursor,
     )
     db.commit()
-    # the end!
+
+
+def extract_ngrams(corpus, rule='{<JJ.*>*<NN.*>+<JJ.*>*}', keys=('title', 'abstract', )):
+    """Extract ngrams for every document below the given corpus.
+    Default language is given by the resource type.
+    The result is then inserted into database.
+    Only fields indicated in `keys` are tagged.
+    """
+    db, cursor = get_cursor()
+    nodes_ngrams_count = defaultdict(int)
+    ngrams_data = set()
+    # extract ngrams
+    resource_type_index = corpus.resources()[0]['type']
+    resource_type = RESOURCETYPES[resource_type_index]
+    default_language_iso2 = resource_type['default_language']
+    for document in corpus.children('DOCUMENT'):
+        # get ngrams extractor for the current document
+        language_iso2 = document.hyperdata.get('language_iso2', default_language_iso2)
+        try:
+            ngramsextractor = ngramsextractors[language_iso2]
+        except KeyError:
+            print('Unrecognized language: `%s`' % (language_iso2, ))
+            continue
+        # extract ngrams on each of the considered keys
+        for key in keys:
+            value = document.hyperdata.get(key, None)
+            if not isinstance(value, str):
+                continue
+            # get ngrams
+            for ngram in ngramsextractor.extract(value):
+                tokens = tuple(token[0] for token in ngram)
+                terms = ' '.join(tokens)
+                nodes_ngrams_count[(document.id, terms)] += 1
+                ngrams_data.add((terms[:255], len(tokens), ))
+        # integrate ngrams and nodes-ngrams
+        if len(nodes_ngrams_count) >= 4096:
+            _integrate_associations(nodes_ngrams_count, ngrams_data, db, cursor)
+            nodes_ngrams_count.clear()
+            ngrams_data.clear()
+    # integrate ngrams and nodes-ngrams
+    _integrate_associations(nodes_ngrams_count, ngrams_data, db, cursor)

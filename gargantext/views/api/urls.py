94ec76968f91d26b73d2b3d33ed4aed68c9da17f
rloth <romain.loth@iscpif.fr>
2016-03-02 15:01:16 +0100

    journals view, with new API route for journal facet, dynatable css factorized (applies for all corpora templates) and menu.html providing a corpora navbar to switch between journals and documents

--- gargantext/views/api/urls.py
+++ gargantext/views/api/urls.py
@@ -6,4 +6,6 @@ from . import nodes
 urlpatterns = [
     url(r'^nodes$', nodes.NodeListResource.as_view()),
     url(r'^nodes/(\d+)$', nodes.NodeResource.as_view()),
+
+    url(r'^nodes/(\d+)/facets$', nodes.CorpusFacet.as_view()),
 ]

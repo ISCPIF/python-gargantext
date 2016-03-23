39b176f95519bfdad5c998001da52cf1376d64f1
rloth <romain.loth@iscpif.fr>
2016-03-23 14:47:24 +0100

    Terms view (with API route for lists and modified js)

--- gargantext/views/api/urls.py
+++ gargantext/views/api/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 
 from . import nodes
+from . import ngramlists
 
 
 urlpatterns = [
@@ -8,4 +9,17 @@ urlpatterns = [
     url(r'^nodes/(\d+)$', nodes.NodeResource.as_view()),
 
     url(r'^nodes/(\d+)/facets$', nodes.CorpusFacet.as_view()),
+
+    # get a list of ngram_ids or ngram_infos by list_id
+    #
+    # url(r'^ngramlists/(\d+)$', ngramlists.List.as_view()),
+
+    # entire combination of lists from a corpus
+    # (or any combination of lists that go together :
+    #   - a mainlist
+    #   - an optional stoplist
+    #   - an optional maplist
+    #   - an optional grouplist
+    # aka lexical model
+    url(r'^ngramlists/family$', ngramlists.ListFamily.as_view()),
 ]

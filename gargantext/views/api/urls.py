f66d6cbf5cb2801455155a94159152675ee3c4fa
Alexandre Delanoë <anoe@debian.org>
2016-04-05 17:01:20 +0200

    [FIX] stashes changes.

--- gargantext/views/api/urls.py
+++ gargantext/views/api/urls.py
@@ -2,26 +2,26 @@ from django.conf.urls import url
 
 from . import nodes
 from . import ngramlists
+from . import scraping
 
+urlpatterns = [ url(r'^nodes$'                , nodes.NodeListResource.as_view())
+              , url(r'^nodes/(\d+)$'          , nodes.NodeResource.as_view()    )
+              , url(r'^nodes/(\d+)/facets$'   , nodes.CorpusFacet.as_view()     )
+              , url(r'^nodes/(\d+)/having$'   , nodes.NodeListHaving.as_view()  )
 
-urlpatterns = [
-    url(r'^nodes$'              , nodes.NodeListResource.as_view()),
-    url(r'^nodes/(\d+)$'        , nodes.NodeResource.as_view()),
+                # get a list of ngram_ids or ngram_infos by list_id
+                # url(r'^ngramlists/(\d+)$', ngramlists.List.as_view()),
 
-    url(r'^nodes/(\d+)/facets$' , nodes.CorpusFacet.as_view()),
-    url(r'^nodes/(\d+)/having$' , nodes.NodeListHaving.as_view()),
+              , url(r'^ngramlists/family$'     , ngramlists.ListFamily.as_view())
+                # entire combination of lists from a corpus
+                # (or any combination of lists that go together :
+                #   - a mainlist
+                #   - an optional stoplist
+                #   - an optional maplist
+                #   - an optional grouplist
+                # aka lexical model
 
-    # add or remove ngram from a list
-    #  ex: add <=> PUT ngramlists/change?list=42&ngrams=1,2
-    #       rm <=> DEL ngramlists/change?list=42&ngrams=1,2
-    url(r'^ngramlists/change$', ngramlists.ListChange.as_view()),
-
-    # get entire combination of lists from a corpus
-    # (or any combination of lists that go together :
-    #   - a mainlist
-    #   - an optional stoplist
-    #   - an optional maplist
-    #   - an optional grouplist)
-    url(r'^ngramlists/family$', ngramlists.ListFamily.as_view()),
-
-]
+              , url(r'^scraping$'              , scraping.Target.as_view()      )
+                # Scraping : getting data from external database
+                # Available databases : Pubmed, IsTex, (next: CERN)
+              ]

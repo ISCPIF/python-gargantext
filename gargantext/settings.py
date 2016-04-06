936658c4bd1a8472543013a3a04dc28a6e258e21
Alexandre Delanoë <anoe@debian.org>
2016-04-06 14:56:48 +0200

    [FEAT] adding scrapers, maybe to add inside the API.

--- gargantext/settings.py
+++ gargantext/settings.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
     'djcelery',
     'annotations',
     'graphExplorer',
-    'scrappers',
+    'scrapers',
 ]
 
 MIDDLEWARE_CLASSES = [
---
+++ scrapers/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url
+
+import scrapers.pubmed as pubmed
+#import scrapers.istex as istex
+#import scrapers.cern  as cern
+#import scrapers.hal   as hal
+
+
+# Scraping : getting data from external database
+# Available databases : Pubmed, IsTex, (next: CERN)
+
+# /!\ urls patterns here are *without* the trailing slash
+urlpatterns = [ url(r'^pubmed/query$', pubmed.getGlobalStats)
+            #, url(r'^scraping$'              , scraping.Target.as_view()      )
+              ,
+              ]

355f8c3a3c81e0fb5452be5fa6729d7eefca393c
Alexandre Delanoë <anoe@debian.org>
2016-04-19 14:59:43 +0200

    [FIX] Docker file for installation.

--- install/debian/asRoot.sh
+++ install/debian/asRoot.sh
@@ -47,6 +47,8 @@ apt-get update && apt-get install -y \
 
 for dir in "/srv/gargantext"\
            "/srv/gargantext_lib"\
+           "/srv/gargantext_static"\
+           "/srv/gargantext_media"\
            "/srv/env_3-5"\
            "/var/www/gargantext"; do \
     mkdir $dir

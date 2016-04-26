5c234cfa5e8f14649f987abd8846b14bfd586db5
Alexandre Delanoë <anoe@debian.org>
2016-04-26 08:52:56 +0200

    [INSTALL] Docker file.

---
+++ install/docker/dev/launch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+sudo docker run -i -p 8000:8000 \
+                   -v /srv:/srv \
+                   -v /home/alexandre:/home/alexandre \
+                   -t gargantext:latest \
+                   /bin/bash
+
+#sudo docker run -i --name srv -v /srv:/srv -t gargantext:latest /bin/bash

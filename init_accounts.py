b2b473b10ecc404999e9b71f0fd33dc718c75e09
Alexandre Delanoë <anoe@debian.org>
2016-04-28 09:27:20 +0200

    [INSTALL] Default account created with init script.

--- init_accounts.py
+++ init_accounts.py
@@ -2,11 +2,45 @@
 import sys
 import os
 
+dirname = os.path.dirname(os.path.realpath(__file__))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gargantext.settings")
+
+# initialize Django application
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+
+
+from django.contrib.auth import password_validation
+from django.contrib.auth.hashers import ( check_password
+                                        , is_password_usable
+                                        , make_password
+                                        )
+from django.db import models
+from django.utils.crypto import get_random_string, salted_hmac
+
+
+# retrieve Django models
+import django.apps
+django_models = django.apps.apps.get_models()
+django_models_names = set(model._meta.db_table for model in django_models)
+
+
 from gargantext.util.db      import session
 from gargantext.models.users import User
 from django.core.mail        import send_mail
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gargantext.settings")
+def make_random_password(length=10
+                         , allowed_chars='abcdefghjkmnpqrstuvwxyz'
+                                         'ABCDEFGHJKLMNPQRSTUVWXYZ'
+                                         '0123456789'):
+    """
+    Generate a random password with the given length and given
+    allowed_chars. The default value of allowed_chars does not have "I" or
+    "O" or letters and digits that look similar -- just to avoid confusion.
+    (source: django/contrib/auth)
+    """
+    return get_random_string(length, allowed_chars)
+
 
 def notify_user(username, email, password):
     message = '''
@@ -42,10 +76,10 @@ def create_user(username, email, user=None, password=None, active=False, notify=
     user.email     = email
     user.is_active = True
     if password is None or password == "":
-        password = User.objects.make_random_password()
-        #print(password)
-    user.set_password(password)
-    user.save()
+        password = make_random_password()
+    user.password = make_password(password)
+    session.add(user)
+    session.commit()
 
     if notify == True:
         notify_user(username, email, password)

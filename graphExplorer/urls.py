a5449bd7e3306f532cedd17bf1c8a444a434a5d1
Alexandre Delanoë <anoe@debian.org>
2016-05-05 08:02:38 +0200

    [URLS] Cleaning the urls, adding goals to factor graph urls (+ fix a deprecated function in distances.py)

--- graphExplorer/urls.py
+++ graphExplorer/urls.py
@@ -1,10 +1,22 @@
 from django.conf.urls import patterns, url
-from graphExplorer import views
 
-# /!\ urls patterns here are *without* the trailing slash
+#from graphExplorer import views
+from graphExplorer.intersection import getCorpusIntersection
 
-urlpatterns = patterns('',
-    url(r'^register/$', views.Register.as_view()), # Register
-    url(r'^login/$', views.Login.as_view()),       # Login
+# Module "Graph Explorer"
+from graphExplorer.rest         import Graph
+from graphExplorer.views        import explorer
+from graphExplorer.intersection import getCorpusIntersection
 
-)
+
+# TODO : factor urls
+# url will have this pattern:
+# ^explorer/$corpus_id/view
+# ^explorer/$corpus_id/data.json
+# ^explorer/$corpus_id/intersection
+
+urlpatterns = [ url(r'^explorer/intersection/(\w+)$', getCorpusIntersection     )
+              , url(r'^projects/(\d+)/corpora/(\d+)/explorer$', explorer       )
+              , url(r'^projects/(\d+)/corpora/(\d+)/graph$'   , Graph.as_view())
+              , url(r'^projects/(\d+)/corpora/(\d+)/node_link.json$', Graph.as_view())
+              ]

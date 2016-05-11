f45c7e96800601eb085c2ad51d1df47ff4a96555
c24b <c24b@protonmail.com>
2016-05-11 18:55:16 +0200

    Check file format error if wrong extension file

--- gargantext/util/files.py
+++ gargantext/util/files.py
@@ -23,9 +23,15 @@ def download(url, name=''):
         basedir = DOWNLOAD_DIRECTORY,
     )
 
+def check_format(corpus_type, name):
+    #~ if True:
+    acc_formats = RESOURCETYPES[corpus_type]["accepted_formats"]
+    if name.split(".")[-1].lower() not in acc_formats:
+        raise TypeError('Uncorrect format of file. File must be a %s file' %" or ".join(acc_formats))
+
 
 def upload(uploaded):
-    print(repr(uploaded))
+
     if uploaded.size > UPLOAD_LIMIT:
         raise IOError('Uploaded file is bigger than allowed: %d > %d' % (
             uploaded.size,

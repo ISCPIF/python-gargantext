195ee7bf97b95a34c4b8c63252478a4264878787
Alexandre Delanoë <anoe@debian.org>
2016-06-28 10:57:00 +0200

    [CONF] enabling axa in urls.

--- gargantext/settings.py
+++ gargantext/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = '!%ktkh981)piil1%t5r0g4$^0=uvdafk!=f2x8djxy7_gq(n5%'
 DEBUG = True
 MAINTENANCE = False
 
-ALLOWED_HOSTS = ['localhost','dev.gargantext.org', 'garg-dev.iscpif.fr','gargantext.org', 'www.gargantext.org']
+ALLOWED_HOSTS = [ 'localhost'
+                , 'gargantext.org'       , 'www.gargantext.org'
+                , 'dev.gargantext.org'   , 'garg-dev.iscpif.fr'
+                , 'emines.gargantext.org', 'axa.gargantext.org'
+                ]
 
 
 # Asynchronous tasks

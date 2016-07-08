e04e0b51cfe3af93af9a6045316d5f3ebb8d21da
rloth <romain.loth@iscpif.fr>
2016-07-08 19:29:48 +0200

    [FEAT] new compact format for graph service <=> explorer exchanges

--- graph/rest.py
+++ graph/rest.py
@@ -14,12 +14,37 @@ def compress_graph(graphdata):
       "nodes": [{"id":4103, "type":"terms", "attributes":{"clust_default": 0}, "size":29, "label":"regard"},...]
       "links": [{"t": 998,"s": 768,"w": 0.0425531914893617},...]
 
-    To send this data over the net, this function can reduce some of its size:
+    To send this data over the net, this function can reduce a lot of its size:
       - keep less decimals for float value of each link's weight
-      - (TODO) have shorter names for node properties (eg: s/clust_default/cd/)
+      - use shorter names for node properties (eg: s/clust_default/cl/)
+
+    result format:
+        "nodes": [{"id":4103, "at":{"cl": 0}, "s":29, "lb":"regard"},...]
+        "links": [{"t": 998,"s": 768,"w": 0.042},...]
     """
     for link in graphdata['links']:
         link['w'] = format(link['w'], '.3f')   # keep only 3 decimals
+
+    for node in graphdata['nodes']:
+        node['lb'] = node['label']
+        del node['label']
+
+        node['at'] = node['attributes']
+        del node['attributes']
+
+        node['at']['cl'] = node['at']['clust_default']
+        del node['at']['clust_default']
+
+        node['s'] = node['size']
+        del node['size']
+
+        if node['type'] == "terms":
+            # its the default type for our format: so we don't need it
+            del node['type']
+        else:
+            node['t'] = node['type']
+            del node['type']
+
     return graphdata
 
 # TODO check authentication

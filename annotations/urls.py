55b9acc43d48a7c0f828125dff591d4f485ae7b4
rloth <romain.loth@iscpif.fr>
2016-07-22 18:54:00 +0200

    [FIX] reconnect the context menu 'add ngram' in annotations view

--- annotations/urls.py
+++ annotations/urls.py
@@ -18,5 +18,5 @@ urlpatterns = [
 
     # 2016-03-24: refactoring, deactivated NgramEdit and NgramCreate
     # 2016-05-27: removed NgramEdit: replaced the local httpservice by api/ngramlists
-    # url(r'^lists/(?P<list_id>[0-9]+)/ngrams/create$', views.NgramCreate.as_view()), #
+    # 2016-07-21: removed NgramCreate: replaced the local httpservice by api/ngrams (put)
 ]

e88e4d2f25fc72351ad79931dbf9b52cc6e5eef4
c24b <c24b@protonmail.com>
2016-07-27 18:08:09 +0200

    NAMING convention for PARSERS dep

--- gargantext/util/parsers/__init__.py
+++ gargantext/util/parsers/__init__.py
@@ -1,21 +1,17 @@
 import importlib
 from gargantext.constants import RESOURCETYPES
 from gargantext.settings import DEBUG
-#if DEBUG:
-#    print("Loading available PARSERS:")
+if DEBUG:
+    print("Loading available PARSERS:")
 base_parser = "gargantext.util.parsers"
 for resource in RESOURCETYPES:
     if resource["parser"] is not None:
         #parser file is without Parser
-        try:
-            fname = resource["parser"].replace("Parser", "")
-            #parser file is formatted as a title
-            module = base_parser+".%s" %(fname.title())
-            #parser module is has shown in constants
-            parser = importlib.import_module(module)
-            #if DEBUG:
-            #    print("\t-", resource["parser"])
-        #getattr(parser,resource["parser"])
-
-        except Exception as e:
-            print("Check constants.py %s \nLANGUAGES declaration of taggers. Parser %s is not available" %(str(e), resource["parser"]))
+        fname = resource["parser"].replace("Parser", "")
+        #parser file is formatted as a title
+        module = base_parser+".%s" %(fname.upper())
+        #parser module is has shown in constants
+        parser = importlib.import_module(module)
+        if DEBUG:
+            print("\t-", resource["parser"])
+        getattr(parser,resource["parser"])

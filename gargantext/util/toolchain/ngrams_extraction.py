fa1a268ee4c3e9c5e3ad7a7307b15c136ece17c6
c24b <c24b@protonmail.com>
2016-07-27 15:15:58 +0200

    load corresponding tagger without ngramsextractors

--- gargantext/util/toolchain/ngrams_extraction.py
+++ gargantext/util/toolchain/ngrams_extraction.py
@@ -1,7 +1,7 @@
 from gargantext.util.db import *
 from gargantext.models import *
 from gargantext.constants import *
-from gargantext.util.ngramsextractors import ngramsextractors
+#from gargantext.util.ngramsextractors import ngramsextractors
 
 from collections import defaultdict
 from re          import sub
@@ -47,75 +47,83 @@ def extract_ngrams(corpus, keys=('title', 'abstract', ), do_subngrams = DEFAULT_
         nodes_ngrams_count = defaultdict(int)
         ngrams_data = set()
         # extract ngrams
-        resource_type_index = corpus.resources()[0]['type']
+        resource = corpus.resources()[0]
         documents_count = 0
-        resource_type = RESOURCETYPES[resource_type_index]
-        default_language_iso2 = resource_type['default_languages']
+        #load available taggers for source default langage
+        taggers_bots = {lang: load_tagger(lang) for lang in resource['default_languages']}
+        #skipped documents that have an unsupported languages
+        corpus.skipped_docs = [doc.id for doc in enumerate(corpus.children('DOCUMENT')) if doc.hyperdata["language_iso2"] not in resource["default_languages"]]
+        print(set(corpus.languages.keys()).intersection(resource["default_languages"]))
+
+        # if lang_doc in corpus.hyperdata['languages']:
+        #     skipped_lg_infos = corpus.hyperdata['languages'].pop(lang_doc)
+        #     corpus.hyperdata['languages']['__skipped__'][lang_doc] = skipped_lg_infos
+        #     corpus.save_hyperdata()
+        #     session.commit()
+        # continue
         for documents_count, document in enumerate(corpus.children('DOCUMENT')):
-            # get ngrams extractor for the current document
-            language_iso2 = document.hyperdata.get('language_iso2', default_language_iso2)
-            try:
-                # this looks for a tagger in constants.LANGUAGES
-                ngramsextractor = ngramsextractors[language_iso2]
-            except KeyError:
-                # skip document
-                print('Unsupported language: `%s` (doc #%i)' % (language_iso2, document.id))
-                # and remember that for later processes (eg stemming)
-                document.hyperdata['__skipped__'] = 'ngrams_extraction'
-                document.save_hyperdata()
-                session.commit()
-                if language_iso2 in corpus.hyperdata['languages']:
-                    skipped_lg_infos = corpus.hyperdata['languages'].pop(language_iso2)
-                    corpus.hyperdata['languages']['__skipped__'][language_iso2] = skipped_lg_infos
+            lang_doc = document.hyperdata['language_iso2']
+            if document not in corpus.skipped_docs:
+            # if document.id in corpus.skipped_docs:
+            #     # get the langage of the current document
+            #     # skip document
+            #     print('Unsupported language: `%s` (doc #%i)' % (lang_doc, document.id))
+            #     # and remember that for later processes (eg stemming)
+            #     #document.hyperdata['__skipped__'] = 'ngrams_extraction'
+            #     #document.save_hyperdata()
+            #     #session.commit()
+            #     continue
+            #
+            #
+            # else:
+                # extract ngrams on each of the considered keys
+                ngramextractor = taggers_bot[lang_doc]
+
+                for key in keys:
+                    value = document.hyperdata.get(key, None)
+                    if not isinstance(value, str):
+                        continue
+                    # get ngrams
+                    for ngram in ngramsextractor.extract(value):
+                        tokens = tuple(normalize_forms(token[0]) for token in ngram)
+
+                        if do_subngrams:
+                            # ex tokens = ["very", "cool", "exemple"]
+                            #    subterms = [['very', 'cool'],
+                            #                ['very', 'cool', 'exemple'],
+                            #                ['cool', 'exemple']]
+
+                            subterms = subsequences(tokens)
+                        else:
+                            subterms = [tokens]
+
+                        for seqterm in subterms:
+                            ngram = ' '.join(seqterm)
+                            if len(ngram) > 1:
+                                # doc <=> ngram index
+                                nodes_ngrams_count[(document.id, ngram)] += 1
+                                # add fields :   terms          n
+                                ngrams_data.add((ngram[:255], len(seqterm), ))
+
+                # integrate ngrams and nodes-ngrams
+                if len(nodes_ngrams_count) >= BATCH_NGRAMSEXTRACTION_SIZE:
+                    _integrate_associations(nodes_ngrams_count, ngrams_data, db, cursor)
+                    nodes_ngrams_count.clear()
+                    ngrams_data.clear()
+                if documents_count % BATCH_NGRAMSEXTRACTION_SIZE == 0:
+                    corpus.status('Ngrams', progress=documents_count+1)
                     corpus.save_hyperdata()
                     session.commit()
-                continue
-            # extract ngrams on each of the considered keys
-            for key in keys:
-                value = document.hyperdata.get(key, None)
-                if not isinstance(value, str):
-                    continue
-                # get ngrams
-                for ngram in ngramsextractor.extract(value):
-                    tokens = tuple(normalize_forms(token[0]) for token in ngram)
-
-                    if do_subngrams:
-                        # ex tokens = ["very", "cool", "exemple"]
-                        #    subterms = [['very', 'cool'],
-                        #                ['very', 'cool', 'exemple'],
-                        #                ['cool', 'exemple']]
-
-                        subterms = subsequences(tokens)
-                    else:
-                        subterms = [tokens]
-
-                    for seqterm in subterms:
-                        ngram = ' '.join(seqterm)
-                        if len(ngram) > 1:
-                            # doc <=> ngram index
-                            nodes_ngrams_count[(document.id, ngram)] += 1
-                            # add fields :   terms          n
-                            ngrams_data.add((ngram[:255], len(seqterm), ))
-
             # integrate ngrams and nodes-ngrams
-            if len(nodes_ngrams_count) >= BATCH_NGRAMSEXTRACTION_SIZE:
-                _integrate_associations(nodes_ngrams_count, ngrams_data, db, cursor)
-                nodes_ngrams_count.clear()
-                ngrams_data.clear()
-            if documents_count % BATCH_NGRAMSEXTRACTION_SIZE == 0:
-                corpus.status('Ngrams', progress=documents_count+1)
-                corpus.save_hyperdata()
-                session.commit()
-        # integrate ngrams and nodes-ngrams
-        _integrate_associations(nodes_ngrams_count, ngrams_data, db, cursor)
-        corpus.status('Ngrams', progress=documents_count+1, complete=True)
-        corpus.save_hyperdata()
-        session.commit()
-    except Exception as error:
-        corpus.status('Ngrams', error=error)
-        corpus.save_hyperdata()
-        session.commit()
-        raise error
+            _integrate_associations(nodes_ngrams_count, ngrams_data, db, cursor)
+            corpus.status('Ngrams', progress=documents_count+1, complete=True)
+            corpus.save_hyperdata()
+            session.commit()
+        except Exception as error:
+            corpus.status('Ngrams', error=error)
+            corpus.save_hyperdata()
+            session.commit()
+            raise error
 
 
 def normalize_forms(term_str, do_lowercase=DEFAULT_ALL_LOWERCASE_FLAG):

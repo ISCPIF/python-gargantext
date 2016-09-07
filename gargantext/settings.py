410ddac610771cda794e62bd07f73a34de08b172
Alexandre Delanoë <anoe@debian.org>
2016-09-07 18:22:41 +0200

    [FIX] Celery ok

--- gargantext/settings.py
+++ gargantext/settings.py
@@ -39,10 +39,13 @@ import djcelery
 djcelery.setup_loader()
 BROKER_URL = 'amqp://guest:guest@localhost:5672/'
 CELERY_ACCEPT_CONTENT = ['pickle', 'json', 'msgpack', 'yaml']
-CELERY_IMPORTS = (  "gargantext.util.toolchain",
+
+CELERY_IMPORTS = (
+                    "gargantext.util.toolchain",
+                    "gargantext.util.crawlers",
                     "graph.cooccurrences",
-                    "gargantext.util.toolchain.main.parse_extract_indexhyperdata",
-                    "gargantext.util.toolchain.main.parse_extract",
+                    "moissonneurs.pubmed",
+                    "moissonneurs.istex",
                     )
 
 

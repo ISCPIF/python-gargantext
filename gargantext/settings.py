e681782741ee64b13372e00d5c88d96bf9aaa670
Alexandre Delanoë <anoe@debian.org>
2016-09-08 17:17:08 +0200

    [FIX] Urls allowed.

--- gargantext/settings.py
+++ gargantext/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '!%ktkh981)piil1%t5r0g4$^0=uvdafk!=f2x8djxy7_gq(n5%'
 DEBUG = True
 MAINTENANCE = False
 
-ALLOWED_HOSTS = [ 'localhost', '.gargantext.org', '.iscpif.fr' ]
+ALLOWED_HOSTS = ["localhost", ".gargantext.org", ".iscpif.fr",]
 
 
 # Asynchronous tasks

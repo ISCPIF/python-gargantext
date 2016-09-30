64ea6b3d4487e7184d111abb083cd5e9945ef93b
Alexandre Delanoë <anoe@debian.org>
2016-09-30 22:44:18 +0200

    Revert "Merge remote-tracking branch 'origin/romain-testing' into testing"
    This reverts commit d9d93ae03792162b3a9f5e0ea5dca68c6b68cd65, reversing
    changes made to 4ef20306dbe38c8afc5508584e6887365c5ab7b1.

--- gargantext/util/db_cache.py
+++ gargantext/util/db_cache.py
@@ -46,9 +46,6 @@ class ModelCache(dict):
 class Cache:
 
     def __getattr__(self, key):
-        '''
-        lazy init of new modelcaches: self.Node, self.User...
-        '''
         try:
             model = getattr(models, key)
         except AttributeError:
@@ -56,16 +53,5 @@ class Cache:
         modelcache = ModelCache(model)
         setattr(self, key, modelcache)
         return modelcache
-
-
-    def clean_all(self):
-        '''
-        re-init any existing modelcaches
-        '''
-        for modelname in self.__dict__:
-            old_modelcache = getattr(cache, modelname)
-            new_modelcache = ModelCache(old_modelcache._model)
-            del old_modelcache
-            setattr(cache, modelname, new_modelcache)
 
 cache = Cache()
--- gargantext/util/toolchain/ngrams_addition.py
+++ gargantext/util/toolchain/ngrams_addition.py
@@ -23,8 +23,6 @@ from gargantext.util.db  import bulk_insert_ifnotexists # £TODO debug
 from sqlalchemy          import distinct
 from re                  import findall, IGNORECASE
 
-from gargantext.util.toolchain.main import t   # timer
-
 # TODO from gargantext.constants import LIST_OF_KEYS_TO_INDEX = title, abstract
 
 def index_new_ngrams(ngram_ids, corpus, keys=('title', 'abstract', )):
@@ -42,8 +40,6 @@ def index_new_ngrams(ngram_ids, corpus, keys=('title', 'abstract', )):
     @param corpus: the CORPUS node
 
     @param keys: the hyperdata fields to index
-
-    # FIXME too slow: index_new_ngrams should be faster via tsvector on DB
     """
 
     # retrieve *all* the ngrams from our list
@@ -60,11 +56,7 @@ def index_new_ngrams(ngram_ids, corpus, keys=('title', 'abstract', )):
     node_ngram_to_write = {}
 
     # loop throught the docs and their text fields
-    for (i, doc) in enumerate(corpus.children('DOCUMENT')):
-
-        if (i % 100 == 0):
-            print('CORPUS #%d: [%s] ngrams_addition: doc %i' % (corpus.id, t(), i))
-            print()
+    for doc in corpus.children('DOCUMENT'):
 
         # a new empty counting subdict
         node_ngram_to_write[doc.id] = {}

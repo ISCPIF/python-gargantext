13177d9dc6c890557c0d48dca438975e3aea7756
c24b <c24b@protonmail.com>
2016-10-10 12:16:12 +0200

    Guided tour js

---
+++ static/lib/gargantext/guided_tour.js
@@ -0,0 +1,29 @@
+modal_tpl = '<div id={modal_id} class="modal">'+
+  <!-- Modal content -->
+  '<div class="modal-content">'+
+  '<span id="cancel" class="close">x</span>'+
+  '<span id="ok" class="submit">x</span>'+
+  '{modal_content}'
+  '</div>'+
+'</div>'
+
+function modalFactory(selector, id, content){
+  $(selector).attr({  "type": "button"});
+
+};
+
+function popOverFactory(selector, pos, confirmation){
+  $(selector).attr({  "type": "button",
+                      "data-toggle": "popover",
+                      "data-placement": pos,
+                      "data-trigger": "toogle",
+
+                    });
+  $(selector).popover({
+    html:true,
+    title: '<h2>'+selector.attr("title")+'</h2>',
+    content: '<p class="text-center">'+selector.attr("content")+'</p>'+
+    '<div class="btn-group"><button class="btn btn-small btn-info" id="ok">Yes</button>'+
+    '<button class="btn btn-small btn-danger" id="close">No</button></div>',
+  });
+};

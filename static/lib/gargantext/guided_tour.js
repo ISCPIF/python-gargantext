42c507b4b72bd6323f02ade19c2bde9656e2f0d1
c24b <c24b@protonmail.com>
2016-10-12 17:04:07 +0200

    [WIP] test on guided tour : popup folies

--- static/lib/gargantext/guided_tour.js
+++ static/lib/gargantext/guided_tour.js
@@ -1,45 +1,6 @@
-var steps = {}
-
-function nextStep(num) {
-  return p[($.inArray(num, p) + 1) % p.length];
-}
-function previousStep(num) {
-  return p[($.inArray(num, p) - 1) % p.length];
-}
-function isPopover(e){
-  if e.hasClass("popover"){
-    return True
-  }
-  else{
-    return False
-  }
-}
-function buildTour(){
-  steps = $(".tour")
-  console.log(steps.length)
-  $(".tour").each(function(i, obj){
-    var me = $(this);
-    //alert($(this));
-    console.log(obj)
-    console.log(i, me.text());
-  });
-};
-
-modal_tpl = '<div id={modal_id} class="modal">'+
-  <!-- Modal content -->
-  '<div class="modal-content">'+
-  '<span id="cancel" class="close">x</span>'+
-  '<span id="ok" class="submit">x</span>'+
-  '{modal_content}'
-  '</div>'+
-'</div>'
-
-function modalFactory(selector, id, content){
-  $(selector).attr({  "type": "button"});
-
-};
-
 function popOverFactory(selector, pos, confirmation){
+  //Factory for popover indicated by class tour
+  // transforming into btn and mapping title and content
   $(selector).attr({  "type": "button",
                       "data-toggle": "popover",
                       "data-placement": pos,
@@ -48,9 +9,52 @@ function popOverFactory(selector, pos, confirmation){
                     });
   $(selector).popover({
     html:true,
-    title: '<h2>'+selector.attr("title")+'</h2>',
+    title: '<h1>'+selector.attr("title")+'</h1>',
     content: '<p class="text-center">'+selector.attr("content")+'</p>'+
-    '<div class="btn-group"><button class="btn btn-small btn-info" id="ok">Yes</button>'+
-    '<button class="btn btn-small btn-danger" id="close">No</button></div>',
+    '<div class="btn-group col-sm-12">'+
+              '<button class="col-sm-6 btn btn-sm btn-danger" data-role="prev">&laquo; Prev</button>'+
+              '<button class="col-sm-6 btn btn-sm  btn-success" data-role="next">Next &raquo;</button>'+
+            '</div><br>',
+    container: 'body',
+    trigger:"toogle"
   });
+  //$('.btn').not(this).popover('hide');
+  //$(selector).popover('toggle');
 };
+
+
+var steps = {}
+
+function nextStep(p, val) {
+  return p[($.inArray(val, p) + 1) % p.length];
+}
+function previousStep(p, num) {
+  return p[($.inArray(val, p) - 1) % p.length];
+}
+function activateTour(){
+  $(document).on("click", "a#guided_tour", function(e){
+    if ($(this).parent().hasClass("active")){
+        $(this).parent().removeClass("active");
+
+        //$(this).popover("hide");
+        $('.popover').popover('hide');
+        //$(this).popover("destroy");
+    }
+    else {
+      $(this).parent().addClass("active");
+      //create the popover
+      popOverFactory($(this), "bottom", true);
+      //hide the other popover
+      $('.popover').not(this).popover('hide');
+      //show the popover
+      $(this).popover('toggle');
+      //buildTour()
+      steps = $(document).find(".tour")
+      console.log(steps)
+      $(".tour").each(function(i, obj, steps){
+          popOverFactory($(this), "right", true)
+          $(this).popover("show");
+
+        })
+      }
+  })};

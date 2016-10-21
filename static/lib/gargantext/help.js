4a4c78582f5d1b0f54f2cd67445fd54125e34a99
c24b <c24b@protonmail.com>
2016-10-21 17:18:19 +0200

    [FIX] filter nav on 1 line

--- static/lib/gargantext/help.js
+++ static/lib/gargantext/help.js
@@ -145,7 +145,7 @@ $(document).on('click', function (e) {
         //the 'is' for buttons that trigger popups
         //the 'has' for icons within a button that triggers a popup
         if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
-            (($(this).popover('hide').data('bs.popover')||{}).inState||{}).click = false  // fix for BS 3.3.6
+            (($(this).popover('hide').data('bs.popover')||{}).inState||{}).click = false  // fix for BS 3.3.6 data-trigger='focus' is a bug
         }
 
     });

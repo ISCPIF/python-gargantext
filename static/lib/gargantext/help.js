baeed314fd35b4bc8f25a9ff057d06df7e76e6b7
c24b <c24b@protonmail.com>
2016-10-25 21:32:07 +0200

    Add help with default user lang

--- static/lib/gargantext/help.js
+++ static/lib/gargantext/help.js
@@ -1,29 +1,37 @@
 //Function that creates contextual help button
-help = {"#project":
-          "en":{},
-          "fr":{
-          "title": "Ajouter un projet",
-          "content":"Cliquez sur le bouton et donnez un nom à votre projet",}
-          ,
-          "placement":"bottom",
-          "position": "inside",
+help = {"#project":{
+          "en":
+            { "title":"Adding a project",
+              "content": "Click on the button and give a name to your projet",
+            },
+          "fr":
+            { "title": "Pour ajouter un projet",
+              "content":"Cliquez sur le bouton et donnez un nom à votre projet",
+            },
+            "placement":"bottom",
+            "position": "inside",
           },
         "#corpus":{
-          "en":{},
-          "fr":{
-            "title": "Ajouter un corpus",
-            "content": "<p>Pour ajouter un corpus à votre projet"+
-                      "<br><b>Selectionnez une base de données</b>"+
-                      "<ol><h4>&#x1f59b; Si vous avez déjà un fichier :</h4>"+
-                      "<li>Vérifier que votre fichier est <a href=''>compressé au format .zip </a> et dans le bon <a href=''>format</a></li>"+
+          "en":
+            { "title":"Adding a project",
+            "content": "Click on the button and give a name to your projet",
+            },
+          "fr":
+            { "title": "Ajouter un corpus",
+              "content": "<p>Pour ajouter un corpus à votre projet"+
+                      "<br><b>Sélectionnez une base de données</b>"+
+                      "<h4><span class='glyphicon glyphicon-hand-left'></span>  Si vous avez déjà un fichier à téléverser :</h4>"+
+                      "<ol>"+
+                      "<li>Vérifiez que votre fichier est <a href=''>compressé (archive .zip) </a> et dans le bon <a href='https://iscpif.fr/gargantext/import-formats'>format</a></li>"+
                       "<li>Cliquez sur 'Choisir un fichier...'</li>"+
                       "<li>Puis donnez un nom à votre corpus</li>"+
                       "<li>Cliquez sur 'Process this!'</li></ol>"+
-                      "<ol><h4>&#x1f59b; Si vous souhaitez faire une recherche:</h4>"+
+                      "<h4><span class='glyphicon glyphicon-hand-left'>  Si vous souhaitez importer un corpus directement depuis une base de donnée ouverte (PubMed ou IsTex pour le moment):</h4>"+
+                      "<ol>"+
                       "<li>Sélectionnez l'option No à la question Do you have a file already?</li>"+
-                      "<li>Entrez votre recherche</li>"+
-                      "<li>Clickez ensuite sur Scan</li>"+
-                      "<li>Une fois le nombre de résultats affichés, clickez sur 'Download!''</li>"+
+                      "<li>Entrez votre requête (la syntaxe de la base de donnée cible est conservée)</li>"+
+                      "<li>Cliquez ensuite sur 'Scan' pour avoir le nombre de résultats de votre requête</li>"+
+                      "<li>Cliquez sur 'Download!' pour importer et analyser un échantillon</li>"+
                       "</ul>"+
                       "</p>",
                     },
@@ -31,40 +39,47 @@ help = {"#project":
           "position": "inside",
           },
           '#docFilter':{
-            "en":{},
-            "fr":{
+            "en":{
+                "title":"Filter document",
+                "content": ""
+                },
+            "fr": {
               "title": "Filtrer les documents",
-              "content": "Vous pouvez afficher ici tous les documents, les favoris ou encore les doublons en selectionnant l'option dans le menu déroulant",
+              "content": "En selectionnant l'option correspondante dans le menu déroulant, vous pouvez afficher ici tous les documents, uniquement vos favoris ou encore rechercher les doublons pour les supprimer",
             },
             "placement":"right",
             "position": "after",
           },
           '#titles_time':{
-            "en":{},
+            "en":{
+              "title":"",
+              "content": ""
+            },
             "fr":{
-            "title": "Filtrer par date",
-            "content": "Selectionnez une plage d'occurence sur une période spécifique en utilisant le curseur sur l'histogramme ci dessus."+
-            "La répartition des documents et leur occurence  pour la période selectionnée s'affiche dans le graphe ci-dessous",
-          },
+              "title": "Filtrer par date",
+              "content": "Selectionnez une période spécifique en utilisant le curseur sur l'histogramme ci-dessus."+
+              "La répartition des documents et leurs occurences pour cette période s'affichera dans la partie inférieure.",
+            },
             "placement": "right",
             "position": "inside",
           },
           '#sources_time':{
             "en":{},
             "fr":{
-              "title": "Filtrer par date",
-              "content": "Selectionnez une plage d'occurence sur une période spécifique en utilisant le curseur sur l'histogramme ci dessus."+
-              "La répartition des sources et leur occurence  pour la péridoe selectionnée s'affiche dans le graphe ci-dessous",
-            }
+              "title": "Filtrer par nombre de publications",
+              "content": "Selectionnez une plage de nombre minimal et maximal de publications par source en utilisant le curseur sur l'histogramme ci-dessus."+
+              "Les sources correspondant à cette plage avec leur nombre de publications s'affichera dans la partie inférieure.",
+            },
             "placement": "right",
             "position": "inside",
           },
           '#terms_time':{
             "en":{},
-            "fr":{"title": "Filtrer par date",
-              "content": "Selectionnez une plage d'occurence sur une période spécifique en utilisant le curseur sur l'histogramme ci dessus."+
-              "La répartition des termes et leur occurence pour la période selectionnée s'affiche dans le graphe ci-dessous",
-            }
+            "fr":{
+              "title": "Filtrer occurrences",
+              "content": "Selectionnez une plage d'occurences en utilisant le curseur sur l'histogramme ci dessus."+
+                         "La liste des termes dont le nombre d'occurrences tombe dans cette place s'affichera dans la partie inférieure.",
+            },
             "placement": "right",
             "position": "inside",
           },
@@ -73,16 +88,17 @@ help = {"#project":
             "fr":{
               "title": "Exporter",
               "content": "Vous pouvez exporter les données <a href=''>format CSV</a>",
-            },
+              },
             "placement": "right",
             "position": "inside",
 
           },
           '#export_terms':{
             "en":{},
-            "fr": { "title": "Exporter",
-                    "content": "Vous pouvez exporter votre liste de termes <a href=''>format CSV</a>",
-                  },
+            "fr":{
+              "title": "Exporter",
+              "content": "Vous pouvez exporter votre liste de termes au format csv. <a href=''>format CSV</a>",
+            },
             "placement": "right",
             "position": "inside",
           },
@@ -97,7 +113,7 @@ help = {"#project":
             "en":{},
             "fr":{
               "title": "Filtrer",
-              "content": "Vous pouvez filtrer vos documents par expression de recherche et consulter leur évolution",
+              "content": "Vous pouvez filtrer les documents contenant un certain type de données (termes, auteurs, etc.) et consulter l'évolution de leur nombre au cours du temps",
             },
             "placement": "right",
             "position": "inside",
@@ -105,36 +121,39 @@ help = {"#project":
           "#filter_terms":{
             "en":{},
             "fr":{
-              "title": "Filtrer",
-              "content": "<p>Vous pouvez filtrer et afficher <ul>les différentes <b>listes</b>:"+
-              "<li>Stop list: tous les termes de la liste qui ont été écartés dans le graphe</li>"+
-              "<li>Map list: tous les termes de la liste qui sont inclus dans le graphe</li>"+
-              "<li>Other: les autres termes non blacklistés et non présents dans le graphe</li></ul></p>"+
-              "<p>Vous pouvez aussi filtrer <ul>les <b>termes</b> en fonction de leur forme:"+
-              "<li>'One-word Terms': selectionner les termes qui ont une forme simple</li>"+
-              "<li>'Multi-word Terms': selectionner les termes qui ont une forme composée</li></ul></p>",
+              "title": "Filtrer les termes",
+              "content": "<p>Vous pouvez filtrer et afficher selectivement les différents types de <b>listes</b>:<ul>"+
+                          "<li><b>Stop list</b>: toutes les expressions identifiées comme non pertinentes a priori (termes creux),</li>"+
+                          "<li><b>Map list</b>: ensemble d'expressions qui consitueront les labels des noeuds de la carte thématique. Chaque label peut potentiellement regrouper plusieurs expressions (ex. singuliers et pluriels).</li>"+
+                          "<li><b>Others</b>: ensemble d'expressions bien formées susceptibles d'être ajoutées à la carte thématique.</li></ul></p>"+
+                          "<p>Vous pouvez aussi filtrer les <b>termes</b> en fonction de leur forme (ils se comportent différemment):<ul>"+
+                          "<li><b>'One-word Terms'</b>: selectionner les termes qui ont une forme simple</li>"+
+                          "<li><b>'Multi-word Terms'</b>: selectionner les termes qui ont une forme composée</li></ul></p>",
             },
             "placement": "right",
             "position": "inside",
           },
           "#filter_graph":{
-            "en":{},
+            "en":{
+              "title":"",
+              "content":"",
+            },
             "fr":{
               "title": "Filtrer",
-              "content": "Filtrer les arcs et les noeuds de votre graphe en fonction de leur poids. Utilisez la barre glissante",
+              "content": "Filtrer les arcs et les noeuds de votre graphe en fonction de leur poids. Utilisez chaque extremité glissante pour retirer les éléments les plus faibles (à gauche) ou les plus forts (à droite)",
             },
             "placement": "right",
             "position":"after",
           }
         }
-
 $( ".help" ).each(function(i, el) {
-  lang = $("a#lang").data("lang")
-  //console.log("This", el)
+
+  console.log("This", el)
   id = el.id
+
   div_id = "#"+id
   help_steps = Object.keys(help)
-  console.log(help_steps)
+  //console.log(help_steps)
   //console.log("div help:", div_id)
   if (help_steps.includes(div_id) == false){
     console.log("Step",id,"not described in help")
@@ -142,9 +161,10 @@ $( ".help" ).each(function(i, el) {
   }
   btn = id+"-help"
   info = help[div_id]
-
-  //console.log(info)
-  help_btn = '<span class="glyphicon glyphicon-question-sign" tab-index=0 data-toggle="popover" data-placement="'+info["placement"]+'"  title="'+info[lang]["title"]+'" data-content="'+info[lang]["content"]+'"></span>'
+  lang = $("a#lang").data("lang")
+  console.log(lang)
+  console.log(info[lang]["content"])
+  help_btn = '<span class="glyphicon glyphicon-question-sign" tab-index=0 data-toggle="popover" data-placement="'+info[lang]["placement"]+'"  title="'+info[lang]["title"]+'" data-content="'+info[lang]["content"]+'"></span>'
 
 
   if (info["position"] == "inside"){
@@ -165,7 +185,6 @@ $( ".help" ).each(function(i, el) {
     console.log(el.parent())
   }
   else{
-    el.attrs()
     //duplicate element and insert the button
     //$(help_btn).insertBefore(el);
   }

a85e4c98e465a9945c1041b44c1b7dadd29d7873
Alexandre Delanoë <anoe@debian.org>
2017-04-13 12:07:23 +0200

    [REPEC/CRAWLER] Fix formular duplicates and number of pages to crawl.

--- gargantext/util/crawlers/MULTIVAC.py
+++ gargantext/util/crawlers/MULTIVAC.py
@@ -61,10 +61,13 @@ class MultivacCrawler(Crawler):
                                    , params  = querystring
                                    )
         
-        print(querystring)
+        #print(querystring)
         # Validation : 200 if ok else raise Value
         if response.status_code == 200:
-            charset = response.headers["Content-Type"].split("; ")[1].split("=")[1]
+            charset = ( response.headers["Content-Type"]
+                                .split("; ")[1]
+                                .split("=" )[1]
+                      )
             return (json.loads(response.content.decode(charset)))
         else:
             raise ValueError(response.status_code, response.reason)
@@ -75,7 +78,12 @@ class MultivacCrawler(Crawler):
         Query String -> Int
         '''
         self.results_nb = 0
-        total = self._get(query)["results"]["total"]
+        
+        total = ( self._get(query)
+                      .get("results", {})
+                      .get("total"  ,  0)
+                )
+        
         self.results_nb = total
 
         return self.results_nb
@@ -89,20 +97,29 @@ class MultivacCrawler(Crawler):
         corpus = []
         paging = 100
         self.query_max = self.scan_results(query)
-        print("self.query_max : %s" % self.query_max)
+        #print("self.query_max : %s" % self.query_max)
 
         if self.query_max > QUERY_SIZE_N_MAX:
-            msg = "Invalid sample size N = %i (max = %i)" % (self.query_max, QUERY_SIZE_N_MAX)
-            print("ERROR (scrap: Multivac d/l ): ",msg)
+            msg = "Invalid sample size N = %i (max = %i)" % ( self.query_max
+                                                            , QUERY_SIZE_N_MAX
+                                                            )
+            print("ERROR (scrap: Multivac d/l ): " , msg)
             self.query_max = QUERY_SIZE_N_MAX
         
-        for page in range(1, trunc(self.query_max / 100) + 1):
+        for page in range(1, trunc(self.query_max / 100) + 2):
             print("Downloading page %s to %s results" % (page, paging))
-            docs = self._get(query, fromPage=page, count=paging)["results"]["hits"]
+            docs = (self._get(query, fromPage=page, count=paging)
+                        .get("results", {})
+                        .get("hits"   , [])
+                   )
+
             for doc in docs:
                 corpus.append(doc)
 
-        self.path = save(json.dumps(corpus).encode("utf-8"), name='Multivac.json', basedir=UPLOAD_DIRECTORY )
+        self.path = save( json.dumps(corpus).encode("utf-8")
+                        , name='Multivac.json'
+                        , basedir=UPLOAD_DIRECTORY
+                        )
         downloaded = True
         
         return downloaded

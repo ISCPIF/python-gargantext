8ce354882497a98e35197ad861cdf9901ff1e3d3
smurail <simon.murail@iscpif.fr>
2017-06-30 17:36:38 +0200

    Add a django custom command callable with manage.py
    Call it with: ./manage nodes show

---
+++ gargantext/management/commands/nodes.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand, CommandError
+from gargantext.tools.show_nodes import tree_show, nodes
+import colorama
+
+
+class Command(BaseCommand):
+    help = 'Nodes'
+
+    def add_arguments(self, parser):
+        parser.add_argument(dest='action', default='show')
+
+    def handle(self, *args, **options):
+        action = options.get('action')
+
+        if action == 'show':
+            colorama.init(strip=False)
+            for root in nodes():
+                tree_show(root)

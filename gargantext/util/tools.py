db2d2e9eae6e4af88b2c64b44a11269496dc3ddf
smurail <simon.murail@iscpif.fr>
2017-07-10 11:56:02 +0200

    [REFACT] Get rid of unused ensure_dir function

--- gargantext/util/tools.py
+++ gargantext/util/tools.py
@@ -9,13 +9,3 @@ def convert_to_date(date):
         return datetime.datetime.timestamp(date)
     else:
         return dateutil.parser.parse(date)
-
-
-def ensure_dir(user):
-    '''
-    If user is new, folder does not exist yet, create it then
-    '''
-    dirpath = '%s/corpora/%s' % (MEDIA_ROOT, user.username)
-    if not os.path.exists(dirpath):
-        print("Creating folder %s" % dirpath)
-        os.makedirs(dirpath)

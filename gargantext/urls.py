ca21f38291250ac52babcf452f4d499b8a0cc8c4
smurail <simon.murail@iscpif.fr>
2017-10-31 16:34:46 +0100

    Remove annotations module

--- gargantext/urls.py
+++ gargantext/urls.py
@@ -4,7 +4,6 @@ Views are shared between these modules:
  - `api`,            for JSON and CSV interaction with data
  - `pages`,          to present HTML views to the user
  - `contents`,       for Python-generated contents
- - `annotations`,    to annotate local context of a corpus (as global context)
 """
 
 from django.conf.urls                   import include, url
@@ -15,11 +14,6 @@ from django.contrib.staticfiles.storage import staticfiles_storage as static
 import gargantext.views.api.urls
 import gargantext.views.pages.urls
 
-# Module Annotation
-    ## tempo: unchanged doc-annotations --
-from gargantext.annotations         import urls as annotations_urls
-from gargantext.annotations.views   import main as annotations_main_view
-
 # Module Scrapers
 import gargantext.moissonneurs.urls
 
@@ -29,12 +23,6 @@ urlpatterns = [ url(r'^admin/'     , admin.site.urls                            
               , url(r'^'           , include( gargantext.views.pages.urls )            )
               , url(r'^favicon.ico$', Redirect.as_view( url=static.url('favicon.ico')
                                     , permanent=False), name="favicon"                 )
-
-              # Module Annotation
-              # tempo: unchanged doc-annotations routes --
-              , url(r'^annotations/', include( annotations_urls )                      )
-              , url(r'^projects/(\d+)/corpora/(\d+)/documents/(\d+)/(focus=[0-9,]+)?$'
-                                                                , annotations_main_view)
 
               # Module Scrapers (Moissonneurs in French)
               , url(r'^moissonneurs/'   , include( gargantext.moissonneurs.urls )      )

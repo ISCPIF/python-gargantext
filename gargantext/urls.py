c7c4be1ba58be785d5ca5edb75fe9a78762f3536
smurail <simon.murail@iscpif.fr>
2017-10-31 17:03:08 +0100

    [REFACT] Little cleanup

--- gargantext/urls.py
+++ gargantext/urls.py
@@ -1,16 +1,13 @@
-"""URL Configuration of GarganText
+"""URL Configuration of GarganText"""
 
-Views are shared between these modules:
- - `contents`,       for Python-generated contents
-"""
-
-from django.conf.urls                   import include, url
+from django.conf.urls                   import url
 from django.contrib                     import admin
 from django.views.generic.base          import RedirectView        as Redirect
 from django.contrib.staticfiles.storage import staticfiles_storage as static
 
 
-urlpatterns = [ url(r'^admin/'     , admin.site.urls                                   )
-              , url(r'^favicon.ico$', Redirect.as_view( url=static.url('favicon.ico')
-                                    , permanent=False), name="favicon"                 )
-              ]
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^favicon.ico$', Redirect.as_view(url=static.url('favicon.ico'),
+                                           permanent=False), name="favicon"),
+]

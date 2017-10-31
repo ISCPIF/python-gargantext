08537e765a2ddf7b56bcbfa23757984d3f3407c5
smurail <simon.murail@iscpif.fr>
2017-10-31 16:43:47 +0100

    Remove old web frontend

--- gargantext/urls.py
+++ gargantext/urls.py
@@ -2,7 +2,6 @@
 
 Views are shared between these modules:
  - `api`,            for JSON and CSV interaction with data
- - `pages`,          to present HTML views to the user
  - `contents`,       for Python-generated contents
 """
 
@@ -12,12 +11,10 @@ from django.views.generic.base          import RedirectView        as Redirect
 from django.contrib.staticfiles.storage import staticfiles_storage as static
 
 import gargantext.views.api.urls
-import gargantext.views.pages.urls
 
 
 urlpatterns = [ url(r'^admin/'     , admin.site.urls                                   )
               , url(r'^api/'       , include( gargantext.views.api.urls )              )
-              , url(r'^'           , include( gargantext.views.pages.urls )            )
               , url(r'^favicon.ico$', Redirect.as_view( url=static.url('favicon.ico')
                                     , permanent=False), name="favicon"                 )
               ]

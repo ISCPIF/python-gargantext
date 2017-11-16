9824c3bd2cd8d6a6a3103bc1ea2a74108c8da2a3
smurail <simon.murail@iscpif.fr>
2017-11-16 18:31:01 +0100

    Restore JWT token route

--- gargantext/backend/urls.py
+++ gargantext/backend/urls.py
@@ -17,9 +17,11 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic.base          import RedirectView        as Redirect
 from django.contrib.staticfiles.storage import staticfiles_storage as static
+from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^favicon.ico$', Redirect.as_view(url=static.url('favicon.ico'),
                                            permanent=False), name="favicon"),
+    url(r'^api/auth/token$', obtain_jwt_token),
 ]

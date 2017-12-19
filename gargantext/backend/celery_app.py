4dbc1fea450459cf6a597b7b94539547fee3f23b
smurail <simon.murail@iscpif.fr>
2017-12-19 11:07:17 +0100

    Typo

--- gargantext/backend/celery_app.py
+++ gargantext/backend/celery_app.py
@@ -1,6 +1,6 @@
 """
-Setup the Celery instance (see also gargantext/__init__.py) that will be
-used by all shared_task.
+Setup the Celery instance (see also gargantext/backend/__init__.py) that will
+be used by all shared_task.
 
 This is the recommended way:
 http://docs.celeryproject.org/en/3.1/django/first-steps-with-django.html
